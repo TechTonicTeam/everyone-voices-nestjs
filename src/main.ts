fad7d91350dbeb237a31e84cb6a43ee729d6cd52
N3WERS <space.galaxy@list.ru>
2024-04-09 16:29:21 +0300

    Added cors, cookie-parser, swagger document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from "cookie-parser";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
-async function bootstrap() {
+async function start() {
+  const PORT = 5000
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  })
+
+  const config = new DocumentBuilder()
+      .setTitle('API build')
+      .addBearerAuth({
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+        description: 'Enter your Bearer token here',
+      }, 'bearer')
+      .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.use(cookieParser())
+  await app.listen(PORT, () => console.log(`SERVER START ON PORT=${PORT}`));
 }
-bootstrap();
+start();
